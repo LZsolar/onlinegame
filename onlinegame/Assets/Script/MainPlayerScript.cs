a0412efcd4c491c3f0d3b2b5efaf2be3549ab04d
LZsolar <pornchanok.char@mail.kmutt.ac.th>
2024-02-11 12:18:45 +0700

    Up to week4

--- onlinegame/Assets/Script/MainPlayerScript.cs
+++ onlinegame/Assets/Script/MainPlayerScript.cs
@@ -2,12 +2,95 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using TMPro;
+using Unity.Collections;
 
 public class MainPlayerScript : NetworkBehaviour
 {
     public float speed = 5.0f;
     public float rotationSpeed = 10.0f;
     Rigidbody rb;
+    public TMP_Text namePrefab;
+    private TMP_Text nameLabel;
+
+    private NetworkVariable<int> posX = new NetworkVariable<int>(
+        0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    public struct NetworkString : INetworkSerializable
+    {
+        public FixedString32Bytes info;
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref info);
+        }
+        public override string ToString()
+        {
+            return info.ToString();
+        }
+        public static implicit operator NetworkString(string v) =>
+            new NetworkString() { info = new FixedString32Bytes(v) };
+    }
+
+    private NetworkVariable<NetworkString> playerNameA = new NetworkVariable<NetworkString>(
+        new NetworkString { info = "Player" },
+        NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    private NetworkVariable<NetworkString> playerNameB = new NetworkVariable<NetworkString>(
+    new NetworkString { info = "Player" },
+    NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    private LoginManagerScript loginManagerScript;
+
+    public override void OnNetworkSpawn()
+    {
+        GameObject canvas = GameObject.FindWithTag("MainCanvas");
+        nameLabel = Instantiate(namePrefab, Vector3.zero, Quaternion.identity) as TMP_Text;
+        nameLabel.transform.SetParent(canvas.transform);
+        posX.OnValueChanged += (int previousValue, int newValue) =>
+        {
+            Debug.Log("Owner ID = " + OwnerClientId + " : Pos X = " + posX.Value);
+        };
+        //if (IsServer)
+        //{
+        //    playerNameA.Value = new NetworkString() { info = new FixedString32Bytes("Player1") };
+        //    playerNameB.Value = new NetworkString() { info = new FixedString32Bytes("Player2") };
+        //}
+        if (IsOwner)
+        {
+            loginManagerScript = GameObject.FindObjectOfType<LoginManagerScript>();
+            if (loginManagerScript != null)
+            {
+                string name = loginManagerScript.userNameInputField.text;
+                if (IsOwnedByServer) { playerNameA.Value = name; }
+                else { playerNameB.Value = name; }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        Vector3 nameLabelPos = Camera.main.WorldToScreenPoint(transform.position + new Vector3(0, 2.5f, 0));
+        nameLabel.text = gameObject.name;
+        nameLabel.transform.position = nameLabelPos;
+        if (IsOwner)
+        {
+            posX.Value = (int)System.Math.Ceiling(transform.position.x);
+        }
+        UpdatePlayerInfo();
+    }
+
+    private void UpdatePlayerInfo()
+    {
+        if (IsOwnedByServer) { nameLabel.text = playerNameA.Value.ToString(); }
+        else { nameLabel.text = playerNameB.Value.ToString(); }
+    }
+
+    public override void OnDestroy()
+    {
+        if (nameLabel != null) Destroy(nameLabel.gameObject);
+        base.OnDestroy();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
